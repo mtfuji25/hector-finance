a2e8bd8adc87629349ff7cfa05eb91bcdbda122b
Kazune Takeda <kazune.takeda@yahoo.com>
2022-01-29 10:04:12 -0500

    Add SVGR

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            typescript: true,
+            memo: true,
+            svgo: false,
+            prettier: false,
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
