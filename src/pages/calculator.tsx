f99af9ebf5e6a3b89447911477186fa4e7cd0019
Kazune Takeda <kazune.takeda@yahoo.com>
2022-03-02 20:25:39 -0500

    Implement Calculator

---
+++ src/pages/calculator.tsx
@@ -0,0 +1,123 @@
+import Head from "next/head";
+import React, { useState, VFC } from "react";
+import { Nav } from "src/components/Nav";
+import {
+  classes,
+  DecimalInput,
+  useDecimalInput,
+  validateEther,
+} from "src/util";
+import { Decimal } from "decimal.js-light";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Calculator — Hector Finance</title>
+      </Head>
+      <div className="flex min-h-screen">
+        <Nav />
+        <main className="flex-grow p-6">
+          <Calculator />
+        </main>
+      </div>
+    </>
+  );
+}
+
+const Calculator: VFC = () => {
+  const [initialHec, initialHecInput, setInitialHecInput] =
+    useDecimalInput("0.01234295");
+  const [apy, apyInput, setApyInput] = useDecimalInput("213.5");
+  const [initialPrice, initialPriceInput, setInitialPriceInput] =
+    useDecimalInput("14.48");
+  const [finalPrice, finalPriceInput, setFinalPriceInput] =
+    useDecimalInput("20.12");
+  const [days, setDays] = useState(30);
+
+  const DAYS_PER_YEAR = 365;
+  const initialUsd = initialHec.times(initialPrice);
+  const apr = apy
+    .div(100)
+    .plus(1)
+    .pow(new Decimal(1).div(DAYS_PER_YEAR))
+    .mul(DAYS_PER_YEAR)
+    .minus(DAYS_PER_YEAR);
+  const finalHec = apr.div(DAYS_PER_YEAR).plus(1).pow(days).times(initialHec);
+  const finalUsd = finalHec.times(finalPrice);
+  const roi = finalHec.minus(initialHec).div(initialHec).times(100);
+
+  return (
+    <div className="max-w-sm space-y-6 text-slate-900">
+      <div>
+        <h1 className="text-2xl">Calculator</h1>
+        <h2>Plan for the future</h2>
+      </div>
+
+      <Input
+        label="sHEC amount"
+        value={initialHecInput}
+        onChange={setInitialHecInput}
+      />
+      <Input label="APY (%)" value={apyInput} onChange={setApyInput} />
+      <Input
+        label="HEC price ($) at purchase"
+        value={initialPriceInput}
+        onChange={setInitialPriceInput}
+      />
+      <Input
+        label={`HEC price ($) after ${days} days`}
+        value={finalPriceInput}
+        onChange={setFinalPriceInput}
+      />
+
+      <label className="block space-y-2">
+        <div>Stake for {days} days</div>
+        <input
+          min={1}
+          max={365}
+          type="range"
+          value={days}
+          onChange={(e) => setDays(e.target.valueAsNumber)}
+          className="block w-full"
+        />
+      </label>
+
+      <div className="space-y-2 rounded bg-emerald-50 p-6 text-emerald-900">
+        <div className="text-xs font-bold uppercase opacity-30">Results</div>
+        <div className="flex justify-between">
+          <div>Initial balance:</div>
+          <div>${initialUsd.toFixed(2)}</div>
+        </div>
+        <div className="flex justify-between">
+          <div>Final balance after {days} days:</div>
+          <div>${finalUsd.toFixed(2)}</div>
+        </div>
+        <div className="flex justify-between">
+          <div>ROI after {days} days:</div>
+          <div>{roi.toFixed(0)}%</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Input: VFC<{
+  label: string;
+  value: DecimalInput;
+  onChange: (input: string) => void;
+}> = ({ label, value, onChange }) => (
+  <label className="block space-y-2">
+    <div>{label}</div>
+    <input
+      className={classes(
+        "w-full rounded px-3 py-2",
+        value.isValid ? "bg-slate-100" : "bg-red-100 text-red-800"
+      )}
+      pattern="[0-9]*"
+      inputMode="decimal"
+      value={value.input}
+      onChange={(e) => onChange(validateEther(e.target.value))}
+    />
+  </label>
+);
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,6 @@
+import { Decimal } from "decimal.js-light";
+import { useMemo, useState } from "react";
+
 /**
  * Build a class string from a list of valid classes.
  * Using this method, you can toggle classes on/off from state.
@@ -33,4 +36,113 @@ export function ok<T, E>(value: T): Result<T, E> {
 
 export function err<E, T>(error: E): Result<T, E> {
   return { isOk: false, error };
+}
+
+export function validateEther(value: string): string {
+  return trimDecimalsPast(18, value);
+}
+
+export function validateMwei(value: string): string {
+  return trimDecimalsPast(6, value);
+}
+
+function trimDecimalsPast(max: number, value: string): string {
+  const decimal = value.indexOf(".");
+  if (decimal === -1) {
+    return value;
+  }
+  return value.slice(0, decimal + 1 + max);
+}
+
+export interface DecimalInput {
+  isValid: boolean;
+  input: string;
+}
+
+export function useDecimalInput(
+  initial: string
+): [Decimal, DecimalInput, (value: string) => void] {
+  const [input, setInput] = useState(initial);
+  return useMemo(() => {
+    const { isValid, decimal } = decimalFromInput(input);
+    return [decimal, { isValid, input }, setInput];
+  }, [input, setInput]);
+}
+
+/**
+ * Transform raw user input into a decimal.
+ *
+ * There are many inputs that are valid to humans but can't
+ * be processed by `Decimal`. This method is useful for adding
+ * some human-friendly leniency to the input parsing.
+ *
+ * You'd definitely expect commas to work in inputs, for example.
+ */
+function decimalFromInput(input: string): {
+  isValid: boolean;
+  decimal: Decimal;
+} {
+  const INVALID_DECIMAL = { isValid: false, decimal: new Decimal("0") };
+
+  input = input.trim();
+
+  if (input === "." || input.length === 0) {
+    // A single decimal point is perfectly valid in my book!
+    // We can interpret it as zero.
+    return { isValid: true, decimal: new Decimal("0") };
+  }
+
+  if (input.match(/[^0-9,\.]/)) {
+    // First ensure only valid characters exist in the input.
+    return INVALID_DECIMAL;
+  }
+
+  let digitsBetweenCommas = 0;
+  let hasDecimal = false;
+  let hasCommas = false;
+  for (const c of input) {
+    if (hasDecimal) {
+      if (c === ",") {
+        // Commas shouldn't appear after decimals.
+        return INVALID_DECIMAL;
+      }
+      if (c === ".") {
+        // Decimals cannot contain more than a single decimal point.
+        return INVALID_DECIMAL;
+      }
+    } else {
+      if (c === ".") {
+        if (hasCommas && digitsBetweenCommas != 3) {
+          // There must be 3 digits between commas and decimal points.
+          return INVALID_DECIMAL;
+        }
+        hasDecimal = true;
+      } else if (c === ",") {
+        if (hasCommas) {
+          if (digitsBetweenCommas != 3) {
+            // There must be 3 digits between commas and decimal points.
+            return INVALID_DECIMAL;
+          }
+        } else {
+          if (digitsBetweenCommas > 3) {
+            // There must be 3 digits between commas and decimal points.
+            return INVALID_DECIMAL;
+          }
+          hasCommas = true;
+        }
+        digitsBetweenCommas = 0;
+      } else {
+        digitsBetweenCommas += 1;
+      }
+
+      if (hasCommas && digitsBetweenCommas > 3) {
+        return INVALID_DECIMAL;
+      }
+    }
+  }
+
+  // Remove all commas because they can't be processed by `Decimal`.
+  input = input.replaceAll(/,/g, "");
+
+  return { isValid: true, decimal: new Decimal(input) };
 }
