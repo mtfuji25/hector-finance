98e0ad2859d91773536514293783fb069f8869c1
Kazune Takeda <kazune.takeda@yahoo.com>
2022-03-02 20:20:18 -0500

    Install decimal.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
+import { Decimal } from "decimal.js-light";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // The default precision of `Decimal` is too low.
+  // We're setting the precision here to affect every page.
+  Decimal.set({ precision: 38 });
+
   return <Component {...pageProps} />;
 }
 
