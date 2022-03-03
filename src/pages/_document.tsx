901eb6e1bb16ab9e91ea6a76c3b82f46aa5e521a
Kazune Takeda <kazune.takeda@yahoo.com>
2022-03-02 20:19:41 -0500

    Change custom document to function component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,17 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from "next/document";
 
-class GlobalDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <link href="/fonts/euclid-square.css" rel="stylesheet" />
-          <link href="/fonts/glacial-indifference.css" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link href="/fonts/euclid-square.css" rel="stylesheet" />
+        <link href="/fonts/glacial-indifference.css" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default GlobalDocument;
