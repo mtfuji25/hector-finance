c739957943b4832d98131c2ae4c08099de458335
Kazune Takeda <kazune.takeda@yahoo.com>
2022-03-04 08:19:32 -0500

    Add favicon links

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,26 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         <link href="/fonts/euclid-square.css" rel="stylesheet" />
         <link href="/fonts/glacial-indifference.css" rel="stylesheet" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon-apple-touch.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon-mask.svg" color="#be7c40" />
       </Head>
       <body>
         <Main />
