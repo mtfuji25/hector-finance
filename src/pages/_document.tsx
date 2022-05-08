9bf0ec351aef1dfce98df2fd0a4ce5074dde2c32
Kazune Takeda <kazune.takeda@yahoo.com>
2022-05-08 05:17:41 -0400

    Replace favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,27 +4,8 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon-apple-touch.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon-mask.svg" color="#be7c40" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
       <body className="overflow-y-scroll font-body text-gray-800">
         <Main />
