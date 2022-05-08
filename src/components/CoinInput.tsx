b8308bf2b596c8998e07109a638dba52eff40abc
Kazune Takeda <kazune.takeda@yahoo.com>
2022-05-08 03:21:01 -0400

    Add wallet icon to coin input balance

--- src/components/CoinInput.tsx
+++ src/components/CoinInput.tsx
@@ -3,6 +3,7 @@ import { StaticImageData } from "next/image";
 import { VFC } from "react";
 import { classes, DecimalInput, validateEther } from "src/util";
 import { StaticImg } from "./StaticImg";
+import WalletIcon from "src/icons/wallet-regular.svgr";
 
 export const CoinInput: VFC<{
   amount: DecimalInput;
@@ -38,7 +39,7 @@ export const CoinInput: VFC<{
             "w-full rounded-t px-3 py-3 pl-11 text-right",
             amount.isValid ? "bg-gray-100" : "bg-red-50 text-red-700",
           )}
-          title={`${tokenName} sell amount`}
+          title={tokenName}
           pattern="[0-9]*"
           inputMode="decimal"
           value={amount.input}
@@ -51,26 +52,31 @@ export const CoinInput: VFC<{
       <div className="h-px bg-transparent" />
       <div className="flex rounded-b bg-gray-100 text-xs text-gray-400">
         <button
-          className="px-3 py-2"
+          className="flex items-center gap-1.5 px-3 py-2"
+          tabIndex={-1}
           onClick={() => onChange(balance.toString())}
         >
+          <WalletIcon className="h-3 w-3 flex-shrink-0 object-contain" />
           {balance.toFixed()}
         </button>
         <div className="flex-grow" />
         <button
           className="p-2 px-3"
+          tabIndex={-1}
           onClick={() => onChange(balance.div(4).toString())}
         >
           1/4
         </button>
         <button
           className="p-2 px-3"
+          tabIndex={-1}
           onClick={() => onChange(balance.div(2).toString())}
         >
           1/2
         </button>
         <button
           className="px-3 py-2"
+          tabIndex={-1}
           onClick={() => onChange(balance.toString())}
         >
           MAX
