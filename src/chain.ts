ec50279f845d7f303e89fd687ccb603bed9675ec
Kazune Takeda <kazune.takeda@yahoo.com>
2022-05-29 00:38:42 -0400

    Add token details to `Chain`

--- src/chain.ts
+++ src/chain.ts
@@ -13,9 +13,7 @@ export type Chain = {
   color: string;
   rpc: [string, ...string[]];
   explorers: string[];
-  token: string;
   logo: StaticImageData;
-
   /**
    * This is a rough estimate of the chain's block time in milliseconds.
    * You should use this when polling the blockchain for changes.
@@ -25,6 +23,14 @@ export type Chain = {
   // dynamically updated. There's no reason to poll more often than
   // the blocks are actually produced.
   millisPerBlock: number;
+
+  token: {
+    name: string;
+    symbol: string;
+    address: string;
+    decimals: number;
+    logo: string;
+  };
 };
 
 export const AVALANCHE: Chain = {
@@ -34,7 +40,13 @@ export const AVALANCHE: Chain = {
   color: "#E84142",
   rpc: ["https://api.avax.network/ext/bc/C/rpc"],
   explorers: ["https://snowtrace.io/"],
-  token: "FvwEAhmxKfeiG8SnEvq42hc6whRyY3EFYAvebMqDNDGCgxN5Z",
+  token: {
+    name: "Avalanche",
+    symbol: "AVAX",
+    address: "FvwEAhmxKfeiG8SnEvq42hc6whRyY3EFYAvebMqDNDGCgxN5Z",
+    logo: "https://assets.coingecko.com/coins/images/12559/small/coin-round-red.png?1604021818",
+    decimals: 18,
+  },
   logo: AvalancheLogo,
   millisPerBlock: 3_500,
 };
@@ -49,7 +61,13 @@ export const BINANCE: Chain = {
     "https://bsc-dataseed1.defibit.io/",
   ],
   explorers: ["https://bscscan.com/"],
-  token: "BNB",
+  token: {
+    name: "BNB",
+    symbol: "BNB",
+    logo: "https://assets.coingecko.com/coins/images/825/small/bnb-icon2_2x.png?1644979850",
+    address: "BNB",
+    decimals: 18,
+  },
   logo: BinanceLogo,
   millisPerBlock: 3_500,
 };
@@ -61,7 +79,13 @@ export const ETHEREUM: Chain = {
   color: "#627EEA",
   rpc: ["https://cloudflare-eth.com", "https://main-rpc.linkpool.io"],
   explorers: ["https://etherscan.io/"],
-  token: "0x0000000000000000000000000000000000000000",
+  token: {
+    name: "Ethereum",
+    symbol: "ETH",
+    logo: "https://assets.coingecko.com/coins/images/279/small/ethereum.png?1595348880",
+    address: "0x0000000000000000000000000000000000000000",
+    decimals: 18,
+  },
   logo: EthereumLogo,
   millisPerBlock: 15_000,
 };
@@ -71,9 +95,15 @@ export const FANTOM: Chain = {
   shortName: "Fantom",
   longName: "Fantom Opera",
   color: "#1969FF",
-  rpc: ["https://rpc.ftm.tools"],
+  rpc: ["https://rpc.fantom.network/", "https://rpc.ftm.tools/"],
   explorers: ["https://ftmscan.com/"],
-  token: "0x4e15361fd6b4bb609fa63c81a2be19d873717870",
+  token: {
+    name: "Fantom",
+    symbol: "FTM",
+    logo: "https://assets.coingecko.com/coins/images/4001/small/Fantom.png?1558015016",
+    address: "0x4e15361fd6b4bb609fa63c81a2be19d873717870",
+    decimals: 18,
+  },
   logo: FantomLogo,
   millisPerBlock: 2_500,
 };
@@ -88,7 +118,13 @@ export const MOONRIVER: Chain = {
     "https://moonriver.api.onfinality.io/public",
   ],
   explorers: ["https://moonriver.moonscan.io/"],
-  token: "0x98878b06940ae243284ca214f92bb71a2b032b8a",
+  token: {
+    name: "Moonriver",
+    logo: "https://assets.coingecko.com/coins/images/17984/small/9285.png?1630028620",
+    symbol: "MOVR",
+    address: "0x98878b06940ae243284ca214f92bb71a2b032b8a",
+    decimals: 18,
+  },
   logo: MoonriverLogo,
   millisPerBlock: 15_000,
 };
@@ -100,7 +136,13 @@ export const POLYGON: Chain = {
   color: "#8247E5",
   rpc: ["https://polygon-rpc.com/", "https://rpc-mainnet.matic.network"],
   explorers: ["https://polygonscan.com/"],
-  token: "0x0000000000000000000000000000000000001010",
+  token: {
+    name: "Polygon",
+    logo: "https://assets.coingecko.com/coins/images/4713/small/matic-token-icon.png?1624446912",
+    symbol: "MATIC",
+    address: "0x0000000000000000000000000000000000001010",
+    decimals: 18,
+  },
   logo: PolygonLogo,
   millisPerBlock: 3_000,
 };
