c70994ee79d4703f4edf239c8b4d7fe72a652a47
Kazune Takeda <kazune.takeda@yahoo.com>
2022-06-10 11:04:02 -0400

    removed manual value from debank

--- src/pages/api/debank.ts
+++ src/pages/api/debank.ts
@@ -217,26 +217,26 @@ function getTreasuryInfo(data: ChainData[]): DeBankData {
     if (deBank.protocols) {
       protocols.push(
         ...deBank.protocols.map((protocol) => {
-          if (protocol.id === "ftm_beefy") {
-            const tempData = protocol.portfolio_item_list.find(
-              (item) => item.pool_id === "temp",
-            );
-            if (!tempData) {
-              protocol.portfolio_item_list.push({
-                detail: { supply_token_list: [] },
-                detail_types: [""],
-                name: "yield",
-                pool_id: "temp",
-                proxy_detail: {},
-                stats: {
-                  asset_usd_value: 18240000,
-                  debt_usd_value: 0,
-                  net_usd_value: 18240000,
-                },
-                update_at: 0,
-              });
-            }
-          }
+          // if (protocol.id === "ftm_beefy") {
+          //   const tempData = protocol.portfolio_item_list.find(
+          //     (item) => item.pool_id === "temp",
+          //   );
+          //   if (!tempData) {
+          //     protocol.portfolio_item_list.push({
+          //       detail: { supply_token_list: [] },
+          //       detail_types: [""],
+          //       name: "yield",
+          //       pool_id: "temp",
+          //       proxy_detail: {},
+          //       stats: {
+          //         asset_usd_value: 18240000,
+          //         debt_usd_value: 0,
+          //         net_usd_value: 18240000,
+          //       },
+          //       update_at: 0,
+          //     });
+          //   }
+          // }
           return {
             ...protocol,
             source: deBank.source,
