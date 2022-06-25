ff209415ab2077cbfe8bc72321ef11953a8a932c
Kazune Takeda <kazune.takeda@yahoo.com>
2022-06-25 04:54:28 -0400

    Fix dark mode text site-wide

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
-      <body className="overflow-y-scroll font-body text-gray-800 dark:bg-gray-900">
+      <body className="overflow-y-scroll font-body text-gray-800 dark:bg-gray-900 dark:text-gray-100">
         <Main />
         <NextScript />
       </body>
